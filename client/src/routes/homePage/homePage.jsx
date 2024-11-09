0c812747b399ddd1887f024c2091383f06d776f3
Samrendra Barick <baricksamarendra@gmail.com>
2024-11-09 22:38:37 +0530

    All Code updated

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Explore homes that make your heart feel at home</h1>
+          <p>
+          Our real estate platform brings a seamless home-buying
+          experience to your fingertips. With an easy-to-use interface, powerful search tools, and real-time updates, we help you find the perfect property for you and your family. Start your journey today!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
